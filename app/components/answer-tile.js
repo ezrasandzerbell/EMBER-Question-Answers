bf7b3f776047a62669d45786033aaea9efc341ca
ezra sandzer-bell <equilibriumarts@gmail.com>
2017-01-13 09:37:55 -0800

    create js logic for answer form and hbs interface for user to submit answer

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  hasAnswer: false,
+  actions: {
+
+    answerQuestion() {
+      this.set('hasAnswer', true);
+    },
+
+    noAnswer() {
+      this.set('hasAnswer', false);
+    },
+
+    saveAnswer() {
+      var params = {
+        answer: this.get('answer'),
+        author: this.get('author'),
+        question: this.get('question')
+      };
+      this.set('answer', '');
+      this.set('author', '');
+      this.sendAction('saveAnswer', params);
+    }
+  }
 });
