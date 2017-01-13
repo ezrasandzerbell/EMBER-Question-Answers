3d9bccf604556094cce8cfffb2e146e4094aa93b
ezra sandzer-bell <equilibriumarts@gmail.com>
2017-01-13 09:28:52 -0800

    display json file content on index page. include links for each question, routes to question_id pages functional.

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  askQuestion: false,
+  actions: {
+
+    newQuestion() {
+      this.set('askQuestion', true);
+    },
+
+    saveQuestion() {
+      var params = {
+        author: this.get('author'),
+        notes: this.get('notes'),
+        question: this.get('question')
+      };
+      this.sendAction('saveQuestion', params);
+      this.set('askQuestion', false);
+    }
+  }
 });
