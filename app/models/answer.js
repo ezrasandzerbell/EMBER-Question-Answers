e915e54d2ce08018482c7b446073ae0cc242f446
ezra sandzer-bell <equilibriumarts@gmail.com>
2017-01-13 11:16:16 -0800

    attempting to link questions/answer. Updated json file and hasmany/belongs to model format

--- app/models/answer.js
+++ app/models/answer.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   author: DS.attr(),
   answer: DS.attr(),
-  question: DS.attr('answer', {async: true})
+  question: DS.belongsTo('question', {async: true})
 });
